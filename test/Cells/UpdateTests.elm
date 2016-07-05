475c58e09ad12bc7d09f260bb01e28b8b7caac7d
Eadred <eadred.birchenough@zuhlke.com>
2016-07-05 15:15:42 +0100

    Recreated cell update unit tests as property tests

--- test/Cells/UpdateTests.elm
+++ test/Cells/UpdateTests.elm
@@ -1,29 +1,46 @@
 module Cells.UpdateTests exposing (tests)
 
 import ElmTest exposing (..)
+import Check as C
+import Check.Producer as P
+import Check.Test as T
 import Cells.Models exposing (..)
 import Cells.Messages exposing (..)
 import Cells.Update exposing (..)
 
-handleBeginTurn : Test
-handleBeginTurn =
-  let model = {status = Empty} in
-  test
-    "beginTurn message should not change model"
-    (assertEqual model (cellUpdate BeginTurn model))
+playerProducer : P.Producer Player
+playerProducer = P.bool |> P.map (\b -> if b then X else O)
 
-handleEndTurn : Test
+cellProducer : P.Producer CellModel
+cellProducer =
+  let choose i =
+    case i of
+      0 -> Empty
+      1 -> Played O
+      _ -> Played X in
+  P.rangeInt 0 2 |> P.map (\i -> {status = choose i})
+
+handleBeginTurn : C.Claim
+handleBeginTurn =
+  C.claim "beginTurn message should not change model"
+    `C.that` (\model -> cellUpdate BeginTurn model)
+    `C.is` identity
+    `C.for` cellProducer
+
+handleEndTurn : C.Claim
 handleEndTurn =
-  let player = X
-      initModel = {status = Empty}
-      expected = {status = Played player} in
-  test
-    "endTurn message should set the cell to Played state"
-    (assertEqual expected (cellUpdate (EndTurn player) initModel))
+  C.claim "endTurn message should set the cell to Played state"
+    `C.that` (\(initModel, player) -> cellUpdate (EndTurn player) initModel)
+    `C.is` (\(_, player) -> {status = Played player})
+    `C.for` (P.tuple (cellProducer, playerProducer))
+
+claims : C.Claim
+claims =
+  C.suite
+    "Cells.Update"
+    [ handleBeginTurn
+    , handleEndTurn
+    ]
 
 tests : Test
-tests =
-  suite "Cells.Update"
-  [ handleBeginTurn
-  , handleEndTurn
-  ]
+tests = claims |> C.quickCheck |> T.evidenceToTest
