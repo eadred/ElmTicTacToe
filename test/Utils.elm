f9b0ed7151a5f7fc743e7fda0638017b6e7c9be2
Eadred <eadred.birchenough@zuhlke.com>
2016-07-06 10:34:46 +0100

    Refactoring of Producer utilities

--- test/Utils.elm
+++ test/Utils.elm
@@ -3,32 +3,20 @@ module Utils exposing (const, oneOf)
 import Check.Producer as P
 import Shrink as S
 import Random as R
+import Random.Extra as RE
 import Lazy.List as L
 
 const : a -> P.Producer a
-const a = P.unit |> P.map (always a)
+const a = P.Producer (RE.constant a) S.noShrink
 
 oneOf : List (P.Producer a) -> P.Producer a
 oneOf ps =
-  let indexed = indexedProducers ps
-       -- Generator (Int, a)
-      generator =
-        (R.int 0 (List.length ps - 1))
-        `R.andThen` (\idx -> (getAt indexed idx).generator)
-       -- Shrinker (Int, a)
-      shrinker (idx,a) = a |> (getAt ps idx).shrinker |> L.map (\a' -> (idx, a'))
-  in P.Producer generator shrinker |> P.map (\(idx,a) -> a)
-
--- Internal functions
-
-indexedProducers : List (P.Producer a) -> List (P.Producer (Int, a))
-indexedProducers =
-  let applyIndex idx = P.map (\a -> (idx,a)) in
-  List.indexedMap applyIndex
-
-getAt : List a -> Int -> a
-getAt ls n =
-  case (ls,n) of
-    ([], _) -> Debug.crash "Reached end of list"
-    ((x::xs), 0) -> x
-    ((x::xs), _) -> getAt xs (n-1)
+  let -- Producer a -> Generator (a, Shrinker a)
+      -- Given a Producer a get a Generator that produces pairs consisting of
+      -- a value and a shrinker to apply to that value
+      toGenAndShrinker p = p.generator |> R.map (\a -> (a, p.shrinker))
+      -- Generator (a, Shrinker a)
+      generator = ps |> List.map toGenAndShrinker |> RE.choices
+       -- Shrinker (a, Shrinker a)
+      shrinker (a, subshrinker) = subshrinker a |> L.map (\a' -> (a', subshrinker))
+  in P.Producer generator shrinker |> P.map (\(a, s) -> a)
