83053df42e38b9e745a32ed468472d4ba3872125
Eadred <eadred.birchenough@zuhlke.com>
2016-07-12 09:29:11 +0100

    Simplified Utils.oneOf since we would haave lost the shrinker in the call to Producer.map anyway.

--- test/Utils.elm
+++ test/Utils.elm
@@ -11,12 +11,5 @@ const a = P.Producer (RE.constant a) S.noShrink
 
 oneOf : List (P.Producer a) -> P.Producer a
 oneOf ps =
-  let -- Producer a -> Generator (a, Shrinker a)
-      -- Given a Producer a get a Generator that produces pairs consisting of
-      -- a value and a shrinker to apply to that value
-      toGenAndShrinker p = p.generator |> R.map (\a -> (a, p.shrinker))
-      -- Generator (a, Shrinker a)
-      generator = ps |> List.map toGenAndShrinker |> RE.choices
-       -- Shrinker (a, Shrinker a)
-      shrinker (a, subshrinker) = subshrinker a |> L.map (\a' -> (a', subshrinker))
-  in P.Producer generator shrinker |> P.map (\(a, s) -> a)
+  let generator = ps |> List.map (\p -> p.generator) |> RE.choices
+  in P.Producer generator S.noShrink
